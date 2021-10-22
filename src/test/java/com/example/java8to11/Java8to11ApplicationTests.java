f70680b2dcfe378a12fb4805cc4a0628cee103f9
yoonmin-kim <rladbsals23@gmail.com>
2021-10-22 22:26:13 +0900

    feat: 자바 8 API의 기본 메소드와 스태틱 메소드
    Iterable의 기본 메소드:
    - forEach()
    - sepliterator()
    
    Collection의 기본 메소드:
    - stream()/ parallelStream()
    - removeIf(Predicate)
    - spliterator()
    
    Comparator의 기본 메소드 및 스태틱 메소드:
    - reversed()
    - thenComparing()
    - static reverseOrder()/ naturalOrder()
    - static nullFirst()/ nullsLast()
    - static comparing()

--- src/test/java/com/example/java8to11/Java8to11ApplicationTests.java
+++ src/test/java/com/example/java8to11/Java8to11ApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.java8to11;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class Java8to11ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("자바 8 API의 기본 메소드와 스태틱 메소드")
+	void defaultStaticMethod() {
+		List<String> name = new ArrayList<>();
+		name.add("yoonmin");
+		name.add("kim");
+		name.add("first");
+		name.add("second");
+
+
+		// name.forEach(System.out::println);
+
+		// iterator 와 동일하지만 split 기능까지 제공
+		Spliterator<String> spliterator = name.spliterator();
+		Spliterator<String> stringSpliterator = spliterator.trySplit();
+		while (spliterator.tryAdvance(System.out::println));
+		System.out.println("===============================");
+		while (stringSpliterator.tryAdvance(System.out::println));
+
+		System.out.println("===============================");
+		List<String> collect = name.stream().map(s -> s + 1)
+			.filter(s -> s.startsWith("yoon"))
+			.collect(Collectors.toList());
+		collect.forEach(System.out::println);
+
+		System.out.println("===============================");
+		// Predicate 에 해당하는 값 제거
+		name.removeIf(s -> s.startsWith("yoon"));
+		name.forEach(System.out::println);
+
+		System.out.println("===============================");
+		Comparator<String> stringComparator = String::compareToIgnoreCase;
+		name.sort(stringComparator.reversed()); // 역순으로 정렬되도록 기본메소드 제공
+		name.forEach(System.out::println);
 	}
 
 }
