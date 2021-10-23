3cf14c33f8f1414be63afc6ce9fa56e0c04a494b
yoonmin-kim <rladbsals23@gmail.com>
2021-10-23 20:03:29 +0900

    feat: Stream 소개
    [Stream]
    - sequence of elements supporting sequential and parallel aggregate operations
    - 데이터를 담고 있는 저장소 (컬렉션)이 아니다.
    - Funtional in nature, 스트림이 처리하는 데이터 소스를 변경하지 않는다.
    - 스트림으로 처리하는 데이터는 오직 한번만 처리한다.
    - 무제한일 수도 있다. (Short Circuit 메소드를 사용해서 제한할 수 있다.)
    - 중개 오퍼레이션은 근본적으로 lazy 하다.
    - 손쉽게 병렬 처리할 수 있다.
    
    [스트림 파이프라인]
    - 0 또는 다수의 중개 오퍼레이션 (intermediate operation)과 한개의 종료 오퍼레이션 (terminal operation)으로 구성한다.
    - 스트림의 데이터 소스는 오직 터미널 오퍼네이션을 실행할 때에만 처리한다.
    
    [중개 오퍼레이션]
    - Stream을 리턴한다.
    - Stateless / Stateful 오퍼레이션으로 더 상세하게 구분할 수도 있다. (대부분은 Stateless지만 distinct나 sorted 처럼 이전
      소스 데이터를 참조해야 하는 오퍼레이션은 Stateful 오퍼레이션이다.)
    - filter, map, limit, skip, sorted, ...
    
    [종료 오퍼레이션]
    - Stream을 리턴하지 않는다.
    - collect, allMatch, count, forEach, min, max, ...

--- src/test/java/com/example/java8to11/Java8to11ApplicationTests.java
+++ src/test/java/com/example/java8to11/Java8to11ApplicationTests.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Spliterator;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,37 +19,66 @@ class Java8to11ApplicationTests {
 	@Test
 	@DisplayName("자바 8 API의 기본 메소드와 스태틱 메소드")
 	void defaultStaticMethod() {
-		List<String> name = new ArrayList<>();
-		name.add("yoonmin");
-		name.add("kim");
-		name.add("first");
-		name.add("second");
+		List<String> names = new ArrayList<>();
+		names.add("yoonmin");
+		names.add("kim");
+		names.add("first");
+		names.add("second");
 
 
 		// name.forEach(System.out::println);
 
 		// iterator 와 동일하지만 split 기능까지 제공
-		Spliterator<String> spliterator = name.spliterator();
+		Spliterator<String> spliterator = names.spliterator();
 		Spliterator<String> stringSpliterator = spliterator.trySplit();
 		while (spliterator.tryAdvance(System.out::println));
 		System.out.println("===============================");
 		while (stringSpliterator.tryAdvance(System.out::println));
 
 		System.out.println("===============================");
-		List<String> collect = name.stream().map(s -> s + 1)
+		List<String> collect = names.stream().map(s -> s + 1)
 			.filter(s -> s.startsWith("yoon"))
 			.collect(Collectors.toList());
 		collect.forEach(System.out::println);
 
 		System.out.println("===============================");
 		// Predicate 에 해당하는 값 제거
-		name.removeIf(s -> s.startsWith("yoon"));
-		name.forEach(System.out::println);
+		names.removeIf(s -> s.startsWith("yoon"));
+		names.forEach(System.out::println);
 
 		System.out.println("===============================");
 		Comparator<String> stringComparator = String::compareToIgnoreCase;
-		name.sort(stringComparator.reversed()); // 역순으로 정렬되도록 기본메소드 제공
-		name.forEach(System.out::println);
+		names.sort(stringComparator.reversed()); // 역순으로 정렬되도록 기본메소드 제공
+		names.forEach(System.out::println);
+	}
+
+	@Test
+	@DisplayName("Stream 소개")
+	public void streamTest() {
+		List<String> names = new ArrayList<>();
+		names.add("yoonmin");
+		names.add("kim");
+		names.add("first");
+		names.add("second");
+
+		// 스트림 생성시 map()은 실행되지 않는다
+		Stream<String> stringStream = names.stream().map((s) -> {
+			System.out.println("s = " + s);
+			return s.toUpperCase();
+		});
+
+		// 종료오퍼레이션인 collect()를 실해하면 비로소 위에서 선언한 map()이 동작한다
+		List<String> collect = stringStream.collect(Collectors.toList());
+		for (String s : collect) {
+			System.out.println("s = " + s);
+		}
+
+		System.out.println("=========================================");
+
+		// 스트림에서 적용한 toUpperCase()가 원본 데이터에는 영향을 미치지 않는다
+		for (String name : names) {
+			System.out.println("name = " + name);
+		}
 	}
 
 }
