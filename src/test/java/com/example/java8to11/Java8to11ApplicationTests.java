384ae0c9bc1050b2573345bfcf0d5a981768c4f7
yoonmin-kim <rladbsals23@gmail.com>
2021-10-25 22:44:48 +0900

    feat: Stream API
    flatMap():
    - List<List<OnlineClass>> yoonminEvents 일때 yoonminEvents 의 Stream 은 List 다
    - 때문에 각 List 에 대해서 또다시 Stream 을 동작해야 하는데 각요소의 Stream 을 한번에
      할 수 있도록 flat 한 형태로 만들어주는것이 flatMap()이다
    
    Stream.iterate(seed[처음시작], seed를 인자로 받는 UnaryOperator):
    - Stream 의 스태틱 메소드이며 별도의 Stream 을 생성하기 위해 사용한다
    - skip(), limit() 등으로 동작을 제한한다

--- src/test/java/com/example/java8to11/Java8to11ApplicationTests.java
+++ src/test/java/com/example/java8to11/Java8to11ApplicationTests.java
@@ -1,18 +1,16 @@
 package com.example.java8to11;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Spliterator;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 
 class Java8to11ApplicationTests {
 
@@ -79,6 +77,68 @@ class Java8to11ApplicationTests {
 		for (String name : names) {
 			System.out.println("name = " + name);
 		}
+	}
+
+	@Test
+	@DisplayName("Stream API")
+	public void steamAPI() {
+		List<OnlineClass> springClasses = new ArrayList<>();
+		springClasses.add(new OnlineClass(1, "spring boot", true));
+		springClasses.add(new OnlineClass(2, "spring data jpa", true));
+		springClasses.add(new OnlineClass(3, "spring mcv", false));
+		springClasses.add(new OnlineClass(4, "spring core", false));
+		springClasses.add(new OnlineClass(5, "rest api development", false));
+
+		System.out.println("[spring 으로 시작하는 수업]");
+		springClasses.stream()
+			.filter(onlineClasses -> onlineClasses.getTitle().startsWith("spring"))
+			.forEach(onlineClasses -> System.out.println(onlineClasses.getTitle()));
+
+		System.out.println("[close 되지 않은 수업]");
+		springClasses.stream()
+			.filter(Predicate.not(OnlineClass::isClosed))
+			.forEach(onlineClass -> System.out.println(onlineClass.getTitle()));
+
+		System.out.println("[수업 이름만 모아서 스트림 만들기]");
+		springClasses.stream()
+			.map(OnlineClass::getTitle)
+			.forEach(System.out::println);
+
+		List<OnlineClass> javaClasses = new ArrayList<>();
+		javaClasses.add(new OnlineClass(6, "The java Test", true));
+		javaClasses.add(new OnlineClass(7, "The java, Code manipulation", true));
+		javaClasses.add(new OnlineClass(8, "The java, 8 to 11", false));
+
+		List<List<OnlineClass>> yoonminEvents = new ArrayList<>();
+		yoonminEvents.add(springClasses);
+		yoonminEvents.add(javaClasses);
+
+		System.out.println("[두 수업 목록에 들어있는 모든 수업 아이디 출력]");
+		// yoonminEvents.stream()
+		// 	.forEach(ocList -> ocList.stream()
+		// 						.map(OnlineClass::getId)
+		// 						.forEach(System.out::println)
+		// 	);
+		yoonminEvents.stream().flatMap(Collection::stream)
+			.forEach(oc -> System.out.println(oc.getId()));
+
+		System.out.println("[10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만]");
+		Stream.iterate(10, i -> i + 1)
+			.skip(10)
+			.limit(10)
+			.forEach(System.out::println);
+
+		System.out.println("[자바 수업 중에 Test가 들어있는 수업이 있는지 확인]");
+		System.out.println(javaClasses.stream()
+			.anyMatch(oc -> oc.getTitle().contains("Test")));
+
+		System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
+		List<String> springTitles = springClasses.stream()
+			.filter(oc -> oc.getTitle().contains("spring"))
+			.map(OnlineClass::getTitle)
+			.collect(Collectors.toList());
+		springTitles.forEach(System.out::println);
+
 	}
 
 }
